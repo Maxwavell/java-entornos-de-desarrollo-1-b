6aed7c62b4fd0f3b5bf9a0dc25f301fe01c195b6
Maxwavell <Franciscojaviersanchezlozano@gmail.com>
2024-03-21 10:05:34 +0100

    Subida 1

---
+++ modelos/Casa.java
@@ -0,0 +1,84 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Casa {
+
+    /**
+     * Default constructor
+     */
+    public Casa() {
+    }
+
+    /**
+     * 
+     */
+    private int String;
+
+    /**
+     * 
+     */
+    private int numHabitaciones;
+
+    /**
+     * 
+     */
+    public int metrosCuadrados;
+
+    /**
+     * @param direccion 
+     * @param numHabitaciones 
+     * @param metrosCuadrados
+     */
+    public void Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String getDireccion() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * @return
+     */
+    public int getNumHabitaciones() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @param direccion
+     */
+    public void setDireccion(String direccion) {
+        // TODO implement here
+    }
+
+    /**
+     * @param numHabitaciones
+     */
+    public void setNumHabitaciones(int numHabitaciones) {
+        // TODO implement here
+    }
+
+    /**
+     * @param metrosCuadrados
+     */
+    public void setmetrosCuadrados(int metrosCuadrados) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public String toString() {
+        // TODO implement here
+        return "";
+    }
+
+}
